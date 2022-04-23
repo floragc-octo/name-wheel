05f47a56505b19ec82ae2303a6699bd7a0a98e33
FloraGC <floragc@protonmail.com>
2022-04-23 18:30:58 +0200

    feat: init simple names wheel

---
+++ config.js
@@ -0,0 +1,17 @@
+const SELECTOR_CHOICES_LIST_ELEMENT_CLASSNAME = "choice-list_element"
+const SELECTOR_CHOICES_LIST_ID = "choice-list"
+const SELECTOR_CHOICE_MAKER_ID = "choice-maker"
+
+const SELECTOR_CHOICES_LIST = "#choice-list"
+const SELECTOR_CHOICES_LIST_ELEMENT = ".choice-list_element"
+const SELECTOR_CHOICE_MAKER = "#choice-maker"
+const SELECTOR_CHOICES_FORM = "#choice-selection"
+const SELECTOR_CHOICES_INPUT = "#choices"
+const SELECTOR_BOARD = "#wheel-board"
+
+const QUERY_PARAM_CHOICE = "choice"
+
+const SEPARATOR = ";"
+
+const CHOICE_CHOOSEN = "choosen"
+const CHOICE_NOT_CHOOSEN = "not-"+CHOICE_CHOOSEN
